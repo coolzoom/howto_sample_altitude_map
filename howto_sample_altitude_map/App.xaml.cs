c839d49c38cc832bef2b42251e57af2dc90849d1
zz <zz@test.com>
2021-02-28 15:56:47 +0800

    Update App.xaml, App.xaml.cs, and 13 more files...

---
+++ howto_sample_altitude_map/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace howto_sample_altitude_map
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
